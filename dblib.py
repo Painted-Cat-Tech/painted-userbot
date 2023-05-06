9956a7a5232dc52b9d1df7f474e2d24db73332c6
felixyeahh <felixyeah@outlook.com>
2023-05-05 22:50:00 -0700

    little refactor

--- dblib.py
+++ dblib.py
@@ -1,4 +1,3 @@
-import json
 '''
 version 0.2
 In development. UNSAVETY!. CAN BREAK USERBOT!
@@ -8,11 +7,15 @@ Codes:
 2:var does not exists
 3:table does not exists
 '''
-db = str("rootdb.json")
+import contextlib
+import json
+
+db = "rootdb.json"
 
 
 def gettable(name: str):
     global db
+
     try:
         with open(db, "r") as settingsfile:
             settings = json.load(settingsfile)
@@ -36,8 +39,10 @@ def settable(name: str, data: dict) -> int:
 
 def setvalue(name: str, var: str, value) -> int:
     global db
+
     with open(db, "r") as settingsfile:
         settings = json.load(settingsfile)
+        
     if name in settings[name]:
         try:
             settings[name] = value
@@ -50,6 +55,7 @@ def setvalue(name: str, var: str, value) -> int:
 
 def getvalue(name: str, var: str):
     global db
+
     try:
         with open(db, "r") as settingsfile:
             settings = json.load(settingsfile)
@@ -67,72 +73,90 @@ def getlang() -> str:
 
 def setlang(lang: str) -> bool:
     global db
+
     with open(db, "r") as settingsfile:
         settings = json.load(settingsfile)
+
     settings["root"]["lang"] = lang
+
     return 0
 
 
 def create(data: dict, file: str) -> int:
     global db
+
     try:
         name = data["name"]
     except KeyError:
         return 2
     with open(db, "r") as settingsfile:
         settings = json.load(settingsfile)
-    unique=True
+
+    unique = True
+
     for mname in settings["root"]["modulelist"]:
-        try:
+        with contextlib.suppress(KeyError):
             mname[name]
-            unique=False
-        except KeyError:
-            pass
-    if unique==False:
+            unique = False
+
+    if not unique:
         return 1
-    elif unique==True:
-        settings[name] = data
-        settings["root"]["modulelist"].append({name: file})
-        try:
-            settings[name]["description"] = data["description"]
-        except KeyError:
-            settings[name]["description"] = "Description not provided"
-        try:
-            settings[name]["help"] = data["help"]
-        except KeyError:
-            settings[name]["help"] = "Help page not provided"
-        with open(db, "w") as settingsfile:
-            json.dump(settings, settingsfile)
-        return 0
+
+    settings[name] = data
+    settings["root"]["modulelist"].append({name: file})
+
+    try:
+        settings[name]["description"] = data["description"]
+    except KeyError:
+        settings[name]["description"] = "Description not provided"
+
+    try:
+        settings[name]["help"] = data["help"]
+    except KeyError:
+        settings[name]["help"] = "Help page not provided"
+
+    with open(db, "w") as settingsfile:
+        json.dump(settings, settingsfile)
+        
+    return 0
 
 
 def getfnbyname(name: str):
     global db
+
     with open(db, "r") as settingsfile:
         settings = json.load(settingsfile)
+
     del settings[name]
-    for mname in range(0, len(settings["root"]["modulelist"])):
+
+    for mname in range(len(settings["root"]["modulelist"])):
         try:
             fn = settings["root"]["modulelist"][mname][name]
         except KeyError:
             return 2
+            
     return fn
 
 
-def delete(name: str) -> str:
+def delete(name: str) -> str | int:
     global db
+
     with open(db, "r") as settingsfile:
         settings = json.load(settingsfile)
+
     try:
         del settings[name]
     except KeyError:
         return 3
-    for mname in range(0, len(settings["root"]["modulelist"])):
+
+    for mname in range(len(settings["root"]["modulelist"])):
         try:
             fn = settings["root"]["modulelist"][mname][name]
             settings["root"]["modulelist"].pop(mname)
         except KeyError:
             return 3
+
     with open(db, "w") as settingsfile:
         json.dump(settings, settingsfile)
+
     return fn
--- userbot.py
+++ userbot.py
@@ -1,117 +1,139 @@
 #!/usr/bin/env python3
+import os, sys
+import asyncio
+import requests
+import dblib
+
 from pyrogram import Client, filters
+from pyrogram.types import Message
+
 app = Client(
     "my_account",
     plugins=dict(root="plugins")
-    )
+)
+
+def command(cmd: str, prefix: str = "%", only_me: bool = True) -> filters.Filter:
+    command = filters.command(cmd, prefixes=prefix) 
+    me = filters.me if only_me else True
+    return command & me
 
 
-@app.on_message(filters.command("test", prefixes="%") & filters.me)
-async def test(client, message):
+@app.on_message(command("test"))
+async def test(_, message: Message):
     await message.edit_text("Works good")
 
 
-@app.on_message(filters.command("help", prefixes="%") & filters.me)
-async def help(client, message):
-    import os
+@app.on_message(command("help"))
+async def help(_, message: Message):
     try:
         moduleslen = len(os.listdir('plugins'))-1
-    except ValueError:
+    except (ValueError, FileNotFoundError):
         moduleslen = 0
-    except FileNotFoundError:
-        moduleslen = 0
-    await message.edit_text(f"""**Number of modules: {moduleslen}**\n
-**Available commands:** \n
-%restart - restart the userbot \n
-%insmod (link to the module) (module name) - install the module \n
-%delmod (module name) - uninstall the module \n
-%modinfo (module name) - print module info \n
-%modlist - print modules list \n
-%test - check for operability \n
-%shutdown - shutdown the userbot \n
-%info - Userbot information""", parse_mode="markdown")
+
+    await message.edit_text(
+        f"**Number of modules: {moduleslen}**\n"
+        "**Available commands:** \n"
+        "%restart - restart the userbot \n"
+        "%insmod (link to the module) (module name) - install the module \n"
+        "%delmod (module name) - uninstall the module \n"
+        "%modinfo (module name) - print module info \n"
+        "%modlist - print modules list \n"
+        "%test - check for operability \n"
+        "%shutdown - shutdown the userbot \n"
+        "%info - Userbot information", parse_mode="markdown"
+    )
 
 
-@app.on_message(filters.command("restart", prefixes="%") & filters.me)
-async def reboot(client, message):
-    from time import sleep
-    import os
-    import sys
+@app.on_message(command("restart"))
+async def reboot(_, message: Message):
     await message.edit_text("Restarting")
-    sleep(2.0)
+    asyncio.sleep(2.0)
     await message.edit_text("Restarted!")
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
-@app.on_message(filters.command("shutdown", prefixes="%") & filters.me)
-async def shutdown(client, message):
-    import sys
+@app.on_message(command("shutdown"))
+async def shutdown(_, message: Message):
     await message.edit_text("Turned off")
     print("Turned off from chat")
     sys.exit()
 
 
-@app.on_message(filters.command("insmod", prefixes="%") & filters.me)
-async def insmodv2(client, message):
+@app.on_message(command("insmod"))
+async def insmodv2(_, message: Message):
     args = message.text.replace("%insmod ", "").split(" ")
-    import requests
-    import dblib
-    import os
-    import sys
+
     if args[0] == "usage":
-        await message.edit_text("""Usage:\n
-%insmod {module url}\n
-Module class:core\n
-Description:download and install modules.\n
-TODO: depedency resolving\n
-BETA!!!!!""")
+        return await message.edit_text(
+            "Usage:\n"
+            "%insmod {module url}\n"
+            "Module class:core\n"
+            "Description:download and install modules.\n"
+            "TODO: depedency resolving\n" # why did you place your todo inside this message? *_* (felix)
+            "BETA!!!!!"
+        )
+
     elif len(args) < 1:
-        await message.edit_text("I need link for download")
+        return await message.edit_text("I need link for download")
+
     elif len(args) > 2:
-        await message.edit_text("Please, choose ONE module name.")
+        return await message.edit_text("Please, choose ONE module name.")
+
+    if len(args) == 1:
+        link = args[0].split("/")
+        filename = link[len(link)-1]
     else:
-        if len(args) == 1:
-            link = args[0].split("/")
-            filename = link[len(link)-1]
-        else:
-            filename = args[1]+".py"
-        try:
-            os.chdir("plugins")
-        except FileNotFoundError:
-            os.mkdir("plugins")
-            os.chdir("plugins")
-        with open(f"{filename}", "w") as modfile:
-            r = requests.get(args[0])
-            fc = r.content.decode("UTF-8")
-            if "json.load" in fc or "json.dump" in fc or "json.loads" in fc or "json.dumps" in fc:
-                await message.edit_text("""This module can try to access root table with very impotant info. Are you sure? (y/n) (redact me)""")
-                while True:
-                    selection = message.text
-                    if selection == "Y" or selection == "y":
-                        break
-                    elif selection == "N" or selection == "n":
-                        await message.edit_text("Installation has been canceled by user")
-                        os.remove(f"{filename}.py")
-                        return
-            modfile.write(fc)
-        os.chdir("../")
-        sys.path.append('plugins')
-        module = __import__(f"{filename.replace('.py', '')}")
-        moddata = module.datadef()
-        moddata["link"] = args[0]
-        code = dblib.create(moddata, filename)
-        sys.path.remove('plugins')
-        if code == 0:
-            await message.edit_text(f"Module {moddata['name']} installed")
-        elif code == 1:
-            await message.edit_text(f"Module {moddata['name']} already installed")
+        filename = f"{args[1]}.py"
+
+    try:
+        os.chdir("plugins")
+    except FileNotFoundError:
+        os.mkdir("plugins")
+        os.chdir("plugins")
+
+    with open(filename, "w") as file:
+        responce = requests.get(args[0]) # requests is not async #todo: use something async instead
+        fc = responce.content.decode("UTF-8")
+        if (
+            "json.load" in fc 
+            or "json.dump" in fc 
+            or "json.loads" in fc 
+            or "json.dumps" in fc
+        ):
+            await message.edit_text("""This module can try to access root table with very impotant info. Are you sure? (y/n) (redact me)""")
+
+            while True:
+                selection = message.text
+                if selection in ["Y", "y"]:
+                    break
+                elif selection in ["N", "n"]:
+                    await message.edit_text("Installation has been canceled by user")
+                    return os.remove(f"{filename}.py")
+                    
+
+        file.write(fc)
+
+    os.chdir("../")
+    sys.path.append('plugins')
+
+    module = __import__(filename.replace('.py', ''))
+    moddata = module.datadef()
+    moddata["link"] = args[0]
+
+    code = dblib.create(moddata, filename)
+    sys.path.remove('plugins')
+
+    if code == 0:
+        await message.edit_text(f"Module {moddata['name']} installed")
+    elif code == 1:
+        await message.edit_text(f"Module {moddata['name']} already installed")
 
 
 @app.on_message(filters.command("delmod", prefixes="%") & filters.me)
-async def delmod(client, message):
+async def delmod(_, message: Message):
     args = message.text.replace("%delmod ", "")
     import dblib
-    import os
+
     if args == "usage":
         await message.edit_text("""Usage:\n
 %delmod {module name}\n
@@ -127,7 +149,7 @@ BETA!!!!!""")
 
 
 @app.on_message(filters.command("modinfo", prefixes="%") & filters.me)
-async def modinfo(client, message):
+async def modinfo(_, message: Message):
     args = message.text.replace("%modinfo ", "")
     import dblib
     if args == "usage":
@@ -149,7 +171,7 @@ Module commands help:{help}""")
 
 
 @app.on_message(filters.command("modlist", prefixes="%") & filters.me)
-async def modlist(client, message):
+async def modlist(_, message: Message):
     import json
     args = message.text.replace("%modlist ", "")
     if args == "usage":
@@ -158,7 +180,7 @@ async def modlist(client, message):
 Module class:core\nDescription:print list of modules\n
         BETA!!!!!""")
     msgtext = "Module list:\n"
-    db = str("rootdb.json")
+    db = "rootdb.json"
     with open(db, "r") as settingsfile:
         settings = json.load(settingsfile)
     if len(settings["root"]["modulelist"]) == 1:
@@ -170,7 +192,8 @@ Module class:core\nDescription:print list of modules\n
 
 
 @app.on_message(filters.command("info", prefixes="%") & filters.me)
-async def info(client, message):
+async def info(_, message: Message):
     await message.edit_text("[Painted Userbot](https://github.com/Painted-Cat-Tech/painted-userbot)\nVersion:0.3-indev)", parse_mode="markdown")
+
 print("Userbot ready to work") 
 app.run()
